3402cae0b38c3efe389769696e68a18295116005
黄浩 <huanghao@172-1-100-112.lightspeed.brhmal.sbcglobal.net>
2022-03-08 19:48:20 +0800

    update

--- pages/list.js
+++ pages/list.js
@@ -1,10 +1,16 @@
 import Head from 'next/head'
-
+import { readFile } from 'fs'
 export default function List() {
-    return <>
-        <Head>
-            <title>List Page</title>
-        </Head>
-        <div>List page works</div>
-    </>
+    return (
+        <>
+            <Head>
+                <title>List Page</title>
+            </Head>
+            <div>List page works</div>
+        </>
+    )
+}
+
+export async function getStaticProps() {
+
 }
